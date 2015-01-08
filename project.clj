757f8f8852b42742dec1ba943353cf8f37a52be4
Rohit Saxena <aerohit.saxena@gmail.com>
2015-01-08 11:24:34 +0100

    A barebones websocket server which reverses the strings.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject funsocks "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "Fun with websockets."
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.immutant/web "2.0.0-beta1"]
+                 [compojure "1.3.1"]
+                 [ring/ring-core "1.3.2"]
+                 [environ "1.0.0"]]
+  :main funsocks.core)
--- src/funsocks/core.clj
+++ src/funsocks/core.clj
@@ -1,6 +1,29 @@
-(ns funsocks.core)
+(ns funsocks.core
+  (:require [immutant.web            :as web]
+            [immutant.web.websocket  :as ws]
+            [immutant.web.middleware :as web-middleware]
+            [compojure.route         :as route]
+            [ring.util.response :refer (redirect)]
+            [compojure.core     :refer (GET defroutes)]
+            [environ.core       :refer (env)]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def websocket-callbacks
+  {:on-open     (fn [ch handshake]
+                  (ws/send! ch "Ready to reverse the messages."))
+   :on-close    (fn [ch {:keys [code reason]}]
+                  (prn "close code:" code "reason:" reason))
+   :on-message  (fn [ch m]
+                  (prn "Received message:" m)
+                  (ws/send! ch (apply str (reverse m))))})
+
+(defroutes routes
+  (GET "/" {c :context} (redirect (str c "/index.html")))
+  (route/resources "/"))
+
+(defn -main [& args]
+  (web/run
+    (-> routes
+        (web-middleware/wrap-session {:timeout 20})
+        (ws/wrap-websocket websocket-callbacks))
+    (merge {"host" (env :demo-web-host), "port" (env :demo-web-port)}
+           args)))
