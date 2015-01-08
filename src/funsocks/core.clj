2cbc6c795b9ceeeb70a6aec210d12ff30475c66a
Rohit Saxena <aerohit.saxena@gmail.com>
2015-01-08 14:29:51 +0100

    Created a post endpoint which broadcasts messages to all the connected websocket channels.

--- src/funsocks/core.clj
+++ src/funsocks/core.clj
@@ -4,20 +4,36 @@
             [immutant.web.middleware :as web-middleware]
             [compojure.route         :as route]
             [ring.util.response :refer (redirect)]
-            [compojure.core     :refer (GET defroutes)]
-            [environ.core       :refer (env)]))
+            [compojure.core     :refer (GET POST defroutes)]
+            [environ.core       :refer (env)]
+            [clojure.set        :refer (union difference)]))
+
+(defonce connected-channels (atom #{}))
 
 (def websocket-callbacks
   {:on-open     (fn [ch handshake]
+                  (swap! connected-channels union #{ch})
                   (ws/send! ch "Ready to reverse the messages."))
    :on-close    (fn [ch {:keys [code reason]}]
+                  (swap! connected-channels difference #{ch})
                   (prn "close code:" code "reason:" reason))
    :on-message  (fn [ch m]
                   (prn "Received message:" m)
                   (ws/send! ch (apply str (reverse m))))})
 
+(defn broadcast [req]
+  (let [msg (:query-string req)]
+    (prn "Broadcasting" msg)
+    (doall (map (fn [ch] (ws/send! ch msg))
+                @connected-channels))))
+
+(defn postbin [req]
+  (broadcast req)
+  "Broadcast to all the connected channels")
+
 (defroutes routes
   (GET "/" {c :context} (redirect (str c "/index.html")))
+  (POST "/postbin" [] postbin)
   (route/resources "/"))
 
 (defn -main [& args]
